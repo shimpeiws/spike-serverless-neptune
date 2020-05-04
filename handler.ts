2246d9586df3d7718aea555d7450b600002b71ca
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-05-04 23:11:18 +0900

    Connect to neptune

--- handler.ts
+++ handler.ts
@@ -1,11 +1,29 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { driver, structure } from "gremlin";
 
-export const hello: APIGatewayProxyHandler = async (event, context) => {
+const DriverRemoteConnection = driver.DriverRemoteConnection;
+const Graph = structure.Graph;
+
+export const hello: APIGatewayProxyHandler = async (event) => {
+  const dc = new DriverRemoteConnection(
+    `wss://${process.env.NEPTUNE_ENDPOINT}:8182/gremlin`,
+    {}
+  );
+  const graph = new Graph();
+  const g = graph.traversal().withRemote(dc);
+
+  const addRes = await g.addV("hello").property("name", "justin").next();
+  console.info("addRes", addRes);
+
+  const res = await g.V().limit(1).count().next();
+  console.info("res", res);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message:
+        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
       input: event,
     }),
   };
-}
+};
